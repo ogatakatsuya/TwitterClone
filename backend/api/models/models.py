66f0af7cdd3ed47f45c82f31d8ea2c71ed6470e3
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-06-03 16:34:06 +0900

    modelを変更

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -9,19 +9,28 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_name = Column(String(1024))
-    password = Column(String(1024))
-    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    name = Column(String(1024), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), server_default=datetime.now(), nullable=False)
     
     posts = relationship("Post", back_populates="user")
+    password = relationship("Password", back_populates="user")
+    
+class Password(Base):
+    __tablename__ = "password"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    password = Column(String(1024), nullable=False)
+    
+    user_id = Column(Integer, ForeignKey('users.id', name="user_password"), nullable=False)
+    user = relationship("User", back_populates="password")
 
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     text = Column(String(200))
-    created_at = Column(DateTime, default=datetime.now(), nullable=False)
-    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
+    created_at = Column(DateTime, default=datetime.now(), server_default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(), server_default=datetime.now(), onupdate=datetime.now(), nullable=False)
 
     user_id = Column(Integer, ForeignKey('users.id', name="fk_posts_users"), nullable=False)
     user = relationship("User", back_populates="posts")
