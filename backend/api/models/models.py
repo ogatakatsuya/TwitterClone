8ff6fafcf8ba857c70276cd87c7653b02d130b01
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-06-03 12:50:01 +0900

    user,postモデルにcreated_at,updated_atを追加

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from datetime import datetime
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 
 from api.db import Base
@@ -9,14 +11,17 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_name = Column(String(1024))
     password = Column(String(1024))
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
     
-    posts = relationship("Post", back_populates="user")  # Corrected class name to "Post"
+    posts = relationship("Post", back_populates="user")
 
 class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     text = Column(String(200))
+    created_at = Column(DateTime, default=datetime.now(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now(), nullable=False)
 
     user_id = Column(Integer, ForeignKey('users.id', name="fk_posts_users"), nullable=False)
     user = relationship("User", back_populates="posts")
