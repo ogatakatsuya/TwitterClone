de755e1b56a882164b4535b1246a8689292b8b85
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-06-07 21:39:55 +0900

    controllerフォルダの追加とmodelの変更

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -19,7 +19,6 @@ class User(Base):
     
     posts = relationship("Post", back_populates="user")
     password = relationship("Password", back_populates="user", uselist=False)
-    comments = relationship("Comment", back_populates="user")
     
 class Password(Base):
     __tablename__ = "passwords"
@@ -37,20 +36,7 @@ class Post(Base):
     text = Column(String(200))
     created_at = Column(DateTime, default=datetime.now(), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime, default=datetime.now(), server_default=func.now(), onupdate=datetime.now(), nullable=False)
+    parent_id = Column(Integer, nullable=True, default=None)
 
     user_id = Column(Integer, ForeignKey('users.id', name="fk_posts_users"), nullable=False)
     user = relationship("User", back_populates="posts")
-    comments = relationship("Comment", back_populates="post")
-
-class Comment(Base):
-    __tablename__ = "comments"
-    
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    text = Column(String(200), nullable=False)
-    created_at = Column(DateTime, default=datetime.now(), server_default=func.now(), nullable=False)
-    
-    user_id = Column(Integer, ForeignKey("users.id", name="fk_comments_users"), nullable=False)
-    post_id = Column(Integer, ForeignKey("posts.id", name="fk_comments_posts"), nullable=False)
-    
-    user = relationship("User", back_populates="comments")
-    post = relationship("Post", back_populates="comments")
