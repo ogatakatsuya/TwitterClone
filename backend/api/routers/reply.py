7a6505d3e96720adc14bd059f70a44922723e617
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-06-09 19:03:47 +0900

    serviceをrepositoryに変更

--- backend/api/routers/reply.py
+++ backend/api/routers/reply.py
@@ -2,8 +2,8 @@ from fastapi import APIRouter, Cookie, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.schemes.reply import Reply, CreateReply
-from api.services.auth.user import get_current_user_id
-from api.services.reply.reply import create_reply, get_reply_by_parent_id, remove_reply
+from api.repository.auth.user import get_current_user_id
+from api.repository.reply.reply import create_reply, get_reply_by_parent_id, remove_reply
 from api.db import get_db
 
 router = APIRouter()
