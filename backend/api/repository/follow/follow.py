b6a9cb8590266677262039752044655c93b6f604
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-06-13 19:05:30 +0900

    follow機能の実装

--- backend/api/repository/follow/follow.py
+++ backend/api/repository/follow/follow.py
@@ -1,10 +1,33 @@
+from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import func
 
-async def follow():
-    pass
+from api.models.models import Follow
+from api.schemes.follow import FollowBody
 
-async def get_users_following():
-    pass
+async def follow(db: AsyncSession, follow_body: FollowBody):
+    follow_create = Follow(
+        follow_id = follow_body.user_id,
+        followed_id = follow_body.follow_id
+    )
+    db.add(follow_create)
+    await db.flush()
+    return True
 
-async def get_users_followed():
-    pass
+async def count_following_users(db: AsyncSession, user_id: int):
+    result = await db.execute(
+        select(func.count(Follow.id))
+        .where(Follow.follow_id == user_id)
+    )
+    following_num = result.scalar()
+    
+    return following_num
+
+async def count_followed_users(db: AsyncSession, user_id: int):
+    result = await db.execute(
+        select(func.count(Follow.id))
+        .where(Follow.followed_id == user_id)
+    )
+    followed_num = result.scalar()
+    
+    return followed_num
--- backend/api/routers/follow.py
+++ backend/api/routers/follow.py
@@ -2,32 +2,62 @@ from fastapi import Depends, APIRouter, HTTPException, Cookie
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.db import get_db
-from api.schemes.follow import CreateFollow
-from api.repository.follow.follow import follow, get_users_followed, get_users_following
+from api.schemes.follow import CreateFollow, FollowBody
+from api.repository.follow.follow import follow, count_followed_users, count_following_users
 from api.repository.auth.user import get_current_user_id
 
 router = APIRouter()
 
 @router.get("/follow")
-async def get_following_users(
+async def get_my_following_users(
     db: AsyncSession = Depends(get_db),
     access_token: str | None = Cookie(default=None)
 ):
-    pass
+    user_id = await get_current_user_id(db, access_token)
+    following_num = await count_following_users(db, user_id)
+    
+    return following_num
 
 @router.get("/followed")
 async def get_users_following_me(
     db: AsyncSession = Depends(get_db),
     access_token: str | None = Cookie(default=None)
 ):
-    pass
+    user_id = await get_current_user_id(db, access_token)
+    followed_num = await count_followed_users(db, user_id)
+    
+    return followed_num
 
-@router.post("/follow")
+@router.post("/follow/{follow_id}")
 async def follow_user(
-    follow_id: CreateFollow, 
+    follow_id: int, 
     db: AsyncSession = Depends(get_db),
     access_token: str | None = Cookie(default=None)
 ):
     user_id = await get_current_user_id(db, access_token)
+    follow_body = FollowBody(
+        user_id = user_id,
+        follow_id = follow_id
+    )
+    success = await follow(db, follow_body)
+    if success:
+        await db.commit()
+        return {"message": "successfully followed"}
+    else:
+        raise HTTPException(code_status=404, detail="failed to follow") #TODO:ここ実行されないかも，要変更
     
-    pass
+@router.get("/follow/{user_id}")
+async def get_users_following(
+    user_id: int,
+    db: AsyncSession = Depends(get_db)
+): #こっちは自分以外のユーザのフォロー情報を取得するやつ
+    following_num = await count_following_users(db, user_id)
+    return following_num
+
+@router.get("/followed/{user_id}")
+async def get_users_followed(
+    user_id: int,
+    db: AsyncSession = Depends(get_db)
+): #こっちは自分以外のユーザのフォロー情報を取得するやつ
+    followed_num = await count_followed_users(db, user_id)
+    return followed_num
