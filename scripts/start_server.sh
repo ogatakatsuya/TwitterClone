419a0db0359010cb95ffe86f458df042cc0b3893
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-07-08 16:05:00 +0900

    server.shを変更

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,9 @@
 # Frontend build and start
 cd /home/ec2-user/frontend
 sudo npm run build
+sudo systemctl restart nextjs
 
 # Backend start
 cd /home/ec2-user/backend
 /home/ec2-user/.local/bin/poetry run python -m api.migrate_db
+sudo systemctl restart fastapi
