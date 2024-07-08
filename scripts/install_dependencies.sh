e100d266b990908c7f9c7eed612901d93c4a1b52
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-07-08 16:01:09 +0900

    shを変更

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -6,4 +6,4 @@ sudo npm install
 
 # Backend setup
 cd /home/ec2-user/backend
-poetry install
+/home/ec2-user/.local/bin/poetry install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,8 +2,8 @@
 
 # Frontend build and start
 cd /home/ec2-user/frontend
-npm run build
+sudo npm run build
 
 # Backend start
 cd /home/ec2-user/backend
-poetry run python -m api.migrate_db
+/home/ec2-user/.local/bin/poetry run python -m api.migrate_db
