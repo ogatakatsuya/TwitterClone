07ee365909d7d205af640a5f9ebc3375e386f976
Ogata Katsuya <130939004+ogatakatsuya@users.noreply.github.com>
2024-07-08 15:39:22 +0900

    test deploy

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -16,7 +16,7 @@ export default function Home() {
   }, []);
   return (
     <>
-      <h1>hello</h1>
+      <h1>deploy succeeded!</h1>
       <Heading>{data}</Heading> 
       <ToLoginButton />
       <ToRegisterButton />
